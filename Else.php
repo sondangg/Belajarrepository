4f9467fb958ca1bdd406ea9501a6350bcef707ad
sondangg <sondangenjelina8@gmail.com>
2023-04-18 15:40:20 +0700

    Kommit Pertama

---
+++ Else.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$umur = 16;
+
+if ($umur >= 18) {
+    echo "Anda Sudah Dewasa.";
+}else {
+    echo "Anda Belum Dewasa.";
+}
+
+?>
+</body>
+</html>
---
+++ If.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+$umur = 20;
+//example syntax if
+if  ($umur >= 18) {
+    echo "Anda Sudah Dewasa.<br>";
+}
+    echo "Sondang Enjelina G<br>"; 
+    echo "221510005<br>"; 
+?>
+</body>
+</html>
